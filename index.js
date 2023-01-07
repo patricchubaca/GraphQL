6babd467443134b19079cd879cb4ac23fb93264b
patricchubaca <patric_job@outlook.com>
2023-01-06 21:16:23 -0300

    Feat :: Hello GraphQL

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Olá, mundo');
+
+const { graphql, buildSchema } = require ('graphql')
+
+const schema = buildSchema(`
+    type Query {
+        olaMundo: String!
+    }
+`);
+
+const resolver = {
+    olaMundo: () => 'Olá Mundo :)'
+}
+
+graphql({schema, '{olaMundo}', resolver})
+.then (resposta => console.log(resposta));
