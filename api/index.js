df113f54526b1bb06fee8d80a516f233df044b09
patricchubaca <patric_job@outlook.com>
2023-01-11 07:40:51 -0300

    Feat :: Start Stud  GraphQL

--- api/index.js
+++ api/index.js
@@ -2,8 +2,6 @@ const { ApolloServer } = require('apollo-server')
 const userSchema = require('./user/schema/user.graphql')
 const userResolvers = require('./user/resolvers/userResolvers')
 
-
-
 const typeDefs = [userSchema]
 const resolvers = [userResolvers]
 
